11d269deedf30dd9713e959039a1bd975be4a018
Rafael Do Carmo <rafaeldocarmo2010@gmail.com>
2019-04-22 16:48:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var app = require('./config/express-config')();
 var port = process.env.port || 8080;
 
 
-app.get('/',function(req,res){
+app.get('/*',function(req,res){
       res.writeHead(200,{'Content-Type': 'text/html'})
 
-    res.end('./app/index.html')
+    res.end(__dirname+'/app/audiolook/index.html')
 })
 
 app.listen(port); 
