c3d07656c558679a46e9dc8cd2ebbf60b30db616
Rafael Do Carmo <rafaeldocarmo2010@gmail.com>
2019-04-29 00:27:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 var app = require('./config/express-config')();
+var path = require('path')
 
 var port = process.env.port || 8080;
 
 
-app.get('/*',function(req,res){
-      res.writeHead(200,{'Content-Type': 'text/html'})
+app.get('*',function(req,res){
+   res.sendFile(path.join(__dirname+'/app/audiolook/index.html'))
+}).listen(port, function(){
+    console.log(' Servidor Escutando na porta ====> ' +port)
 
-    res.end(__dirname+'/app/audiolook/index.html')
-})
-
-app.listen(port); 
-
-console.log(' Servidor Escutando na porta ====> ' +port)
+}); 
